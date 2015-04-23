b3d57e778ba028068bb2536586276390ed654676
rama21389 <ramiro.e.mendez@hotmail.com>
2015-04-23 11:52:09 -0300

    tp 1 con bool y strings
    use los tipos de datos que no use en la primera version.

---
+++ tp - 1- con bools y strings.cpp
@@ -0,0 +1,27 @@
+/* ramiro mendez
+ *1451613
+ * TP 1 - con bool y strings
+ */
+ 
+ #include <iostream>
+ #include <string>
+ 
+ int main ()
+ {
+ 	using namespace std;
+ 	
+ 	bool b(true);
+ 	
+ 	string ejemplo1 ("hello");
+ 	string ejemplo2 ("world");
+ 	
+ 	cout <<b<<endl;
+ 	cout <<!b<<endl;
+ 	cout << ejemplo1 +" "+ ejemplo2<< endl;
+ 	
+ 	return 0;
+ }
+ 
+
+ 
+ 
